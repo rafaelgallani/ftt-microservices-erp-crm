561e5bccc8a921560a3f876342e7bf65d36d3497
rafaelgalani <galani.rafael@gmail.com>
2020-06-15 00:47:33 -0300

    adding default data to microservices

--- quotes/quotes.py
+++ quotes/quotes.py
@@ -13,6 +13,8 @@ from pricing_service import PricingService, ParsingError
 from nameko.dependency_providers import Config
 
 DEFAULT_PRODUCTS = (0, 1, 2, 123, 100, 200, 300)
+DEFAULT_CUSTOMERS = (0, 1, 2, 123, 100, 200, 300)
+DEFAULT_DELIVERY_TYPES = (0, 1, 2)
 
 amqp_uri = 'amqp://{RABBIT_USER}:{RABBIT_PASSWORD}@{RABBIT_HOST}:{RABBIT_PORT}/'.format(
     RABBIT_USER=os.getenv('RABBIT_USER', 'guest'),
@@ -35,7 +37,7 @@ class QuoteService:
 
     @rpc
     def get(self, quote_id):
-        self.create_default_products()
+        self.setup_default_data()
         quote = self.redis.hgetall(quote_id)
         if 'id' not in quote:
             return None
@@ -52,7 +54,7 @@ class QuoteService:
 
     @rpc
     def create(self, quote_json):
-        self.create_default_products()
+        self.setup_default_data()
         quote_id = uuid.uuid4().hex
 
         quote_as_dict = json.loads(quote_json)
@@ -76,6 +78,11 @@ class QuoteService:
 
         return quote_id
 
+    def setup_default_data(self):
+        self.create_default_products()
+        self.create_default_customers()
+        self.create_default_delivery_types()
+
     def create_default_products(self):
         for product in DEFAULT_PRODUCTS:
             product_id = str(product)
@@ -87,26 +94,46 @@ class QuoteService:
                     "price": 100
                 })
 
-    def handle_quote_data(self, quote_data)
-        parse_products(quote_data['items'])
-        parse_customer(quote_data)
-        parse_delivery_type(quote_data)
+    def create_default_customers(self):
+        for customer in DEFAULT_CUSTOMERS:
+            customer_id = str(customer)
+            record_id = 'customer_'+customer_id
+            
+            if not self.redis.hgetall(record_id):
+                self.redis.hmset(record_id, {
+                    "customerId": customer_id,
+                })
+    
+    def create_default_delivery_types(self):
+        for delivery_type in DEFAULT_DELIVERY_TYPES:
+            delivery_type_id = str(delivery_type)
+            record_id = 'delivery_type_'+delivery_type_id
+            
+            if not self.redis.hgetall(record_id):
+                self.redis.hmset(record_id, {
+                    "deliveryTypeId": delivery_type_id,
+                })
+
+    def handle_quote_data(self, quote_data):
+        self.parse_products(quote_data['items'])
+        self.parse_customer(quote_data)
+        self.parse_delivery_type(quote_data)
     
     def parse_customer(self, quote_data):
         customer_id = str(quote_data['customerId'])
         if not self.redis.hgetall('customer_' + customer_id):
-            raise ParsingError('Invalid customer specified: {customer_id}.'.format(customer_id=customer_id))
+            raise ParsingError('Invalid customer id specified: "{customer_id}".'.format(customer_id=customer_id))
     
     def parse_delivery_type(self, quote_data):
         delivery_type_id = str(quote_data['deliveryTypeId'])
         if not self.redis.hgetall('delivery_type_' + delivery_type_id):
-            raise ParsingError('Invalid delivery type specified: {delivery_type_id}.'.format(delivery_type_id=delivery_type_id))
+            raise ParsingError('Invalid delivery type id specified: "{delivery_type_id}".'.format(delivery_type_id=delivery_type_id))
         
     def parse_products(self, items):
         for index, item in enumerate(items):
             product_id_str = str(item['productId'])
             if not self.redis.hgetall('product_'+product_id_str):
-                raise ParsingError('Invalid product specified in item at {index}: {invalid_product}.'.format(index=index, invalid_product= item['productId']))
+                raise ParsingError('Invalid product id specified on item at {index}: "{invalid_product}".'.format(index=index, invalid_product=item['productId']))
     
     @rpc
     def get_all(self):
